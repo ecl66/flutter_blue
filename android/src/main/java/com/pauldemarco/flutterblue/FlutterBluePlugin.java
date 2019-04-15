fca5d4cf9fc406824352d1fd2067f85f6de9c194
Emmanuel Cohen-Laroque <eclpublic@online.fr>
2019-04-15 08:58:50 +0200

    reinstated android clear

--- android/src/main/java/com/pauldemarco/flutterblue/FlutterBluePlugin.java
+++ android/src/main/java/com/pauldemarco/flutterblue/FlutterBluePlugin.java
@@ -163,7 +163,7 @@ public class FlutterBluePlugin implements MethodCallHandler, RequestPermissionsR
             {
                 mBluetoothAdapter.disable();
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(500);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
